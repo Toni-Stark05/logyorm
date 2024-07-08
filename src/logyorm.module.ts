e2db344da636facaa5b89f2cd9f42f361d1ad4c0
tonistark <nikidysbdbdy@gmail.com>
2024-07-08 13:29:12 +0300

    add forRootAsync

--- src/logyorm.module.ts
+++ src/logyorm.module.ts
@@ -1,6 +1,11 @@
-import { Module, DynamicModule, Global } from '@nestjs/common';
+import { Module, DynamicModule, Global, Provider } from '@nestjs/common';
 import { createConnection, ConnectionOptions, Connection } from './core/connection';
 import { LogyOrmService } from './logyorm.service';
+
+export interface LogyOrmModuleAsyncOptions {
+    useFactory: (...args: any[]) => Promise<ConnectionOptions> | ConnectionOptions;
+    inject?: any[];
+}
 
 @Global()
 @Module({})
@@ -9,6 +14,23 @@ export class LogyOrmModule {
         const connectionProvider = {
             provide: 'DATABASE_CONNECTION',
             useFactory: async () => await createConnection(options),
+        };
+
+        return {
+            module: LogyOrmModule,
+            providers: [connectionProvider, LogyOrmService],
+            exports: [LogyOrmService],
+        };
+    }
+
+    static forRootAsync(options: LogyOrmModuleAsyncOptions): DynamicModule {
+        const connectionProvider: Provider = {
+            provide: 'DATABASE_CONNECTION',
+            useFactory: async (...args: any[]) => {
+                const connectionOptions = await options.useFactory(...args);
+                return await createConnection(connectionOptions);
+            },
+            inject: options.inject || [],
         };
 
         return {
